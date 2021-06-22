9ed7230d525876a8a964a32dbef39912a04c0bdb
Kaushik <kaushikjjj@gmail.com>
2021-06-23 03:19:45 +0530

    relation extractor done

---
+++ README.md
@@ -0,0 +1,5 @@
+# Knowledge Graph Extraction
+
+## Setup
+1. run sh setup.sh
+2. pip install -r requirements.txt
---
+++ setup.sh
@@ -0,0 +1,14 @@
+# downlaod pre trained bert model
+wget -P ./pretrain/nre https://thunlp.oss-cn-qingdao.aliyuncs.com/opennre/pretrain/nre/wiki80_bert_softmax.pth.tar
+
+#download bert
+cd ./pretrain
+sh ./pretrain/download_bert.sh
+cd ..
+
+cd ./benchmark
+#download wiki80
+sh ./benchmark/download_wiki80.sh
+cd ..
+
+python3 setup_python.py
